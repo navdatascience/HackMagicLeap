be436f781a73b35a19ab4c4bc249f0265683cc58
Rico Balakit <rico@balak.it>
2019-09-28 14:31:00 -0500

    heatmap reskinning

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (PlayerController.PlayerConstoller != null)
+        {
+            transform.LookAt(PlayerController.PlayerConstoller.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private static PlayerController s_playerController;
+
+    public static PlayerController PlayerConstoller
+    {
+        get { return s_playerController; }
+    }
+
+    private void Start()
+    {
+        if(s_playerController != null)
+        {
+            Debug.LogError("more than two player controllers initialized!");
+            return;
+        }
+        else
+        {
+            s_playerController = this;
+        }
+    }
+}
