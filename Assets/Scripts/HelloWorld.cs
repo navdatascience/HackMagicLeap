e4c4f626ab2005aa78ded8bcddff96c0806be924
Rico Balakit <rico@balak.it>
2019-09-28 11:54:12 -0500

    my first commit! purple spinny cube!

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Rico says hello!");
+    }
+
+    void Update()
+    {
+        transform.RotateAround(Vector3.up, Time.deltaTime);
+    }
+}
